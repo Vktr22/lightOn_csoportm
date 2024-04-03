7be32bdc1fca465e7428e121a04a57ffae1152dc
Vktr22 <nds.viktoria@gmail.com>
2024-04-03 08:31:50 +0200

    esemeny fgv

--- fuggvenyek.js
+++ fuggvenyek.js
@@ -24,8 +24,42 @@ export function szerkezet(lista){
 }
 
 export function esemeny(lista){
-    const mezoElemek = $("#jatekter div")
+    const mezoElemek = $("#jatekter div");
+
     mezoElemek.on("click", function(event){
-        console.log($(event.target))
-    })
+        const targetIndex = mezoElemek.index($(this)); // Az aktuális div indexének meghatározása
+        const targetDiv = $(this);
+
+        // Függvény a világító osztály hozzáadására vagy eltávolítására
+        function toggleVilagitClass(element) {
+            element.toggleClass("vilagit");
+        }
+
+        function updateSzamlalo() {
+            const vilagitMezokSzama = mezoElemek.filter('.vilagit').length;
+            szamlaloElem.text(vilagitMezokSzama);
+        }
+       
+        const szomszedIndexek = [
+            [1, 3],     // 0
+            [0, 2, 4],     // 1
+            [1, 5],       // 2
+            [0, 4, 6],     // 3
+            [1, 3, 5, 7],  // 4
+            [2, 4, 8],    // 5
+            [3, 7],        // 6
+            [4, 6, 8],   // 7
+            [5, 7]      // 8
+        ];
+
+        // Átkapcsolás a szomszédos mezőkre
+        szomszedIndexek[targetIndex].forEach(szomszedIndex => {
+            toggleVilagitClass(mezoElemek.eq(szomszedIndex));
+        });
+
+        // A kattintott mezőre vonatkozó átkapcsolás
+        toggleVilagitClass(targetDiv);
+
+        lampak()
+    });
 }
